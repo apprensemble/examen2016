c00a99305fa7de8d22804ccbf83b27a0fda49ee8
thierry@moncourriel.eu <thierry>
2016-03-09 14:59:48 +0100

    ex 2

--- src/q2/Q2Main.java
+++ src/q2/Q2Main.java
@@ -1,7 +1,10 @@
 package q2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Q2Main {
 
@@ -14,6 +17,21 @@ public class Q2Main {
 		 * les autres mises dans une même structure. Déterminer la structure à
 		 * utiliser et remplir celle-ci. Afficher les deux listes résultats.
 		 */
+			String sup50 = "sperieur a 50";
+			String autre = "les autres";
+		Map<String, List<Integer>> classement = new HashMap<>();
+				classement.put(sup50,new ArrayList<>());
+				classement.put(autre,new ArrayList<>());
+		lint.forEach(i -> {
+			if (i>50) {
+				classement.get(sup50).add(i);
+			}
+			else {
+				classement.get(autre).add(i);
+			}
+		});
+		System.out.format("superieur a 50 -> %s\n",classement.get(sup50));
+		System.out.format("les autres -> %s\n",classement.get(autre));
 		
 		
 	}
